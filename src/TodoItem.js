3bf51f27c314641e4b24bbde3760bed73c22ecdb
Eduardo Reyes <joeduardoreyes@outlook.com>
2023-11-15 11:07:25 -0600

    Estilos CSS en React

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,9 +3,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
-function TodoItem({ text }) {
+function TodoItem({ text, completed }) {
 	return (
-		<li className="todoItem">
+		<li className={`todoItem ${completed && "completed"}`}>
 			<FontAwesomeIcon icon={faCircleCheck} />
 			<p>{text}</p>
 			<FontAwesomeIcon icon={faXmark} />

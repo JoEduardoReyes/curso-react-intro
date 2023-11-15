d33b1b9a880ba99bdcc6caf395126b747405d6ca
Eduardo Reyes <joeduardoreyes@outlook.com>
2023-11-15 11:07:25 -0600

    🎨 Terminamos los estilos del TodoItem y Create

--- src/CreateTodoButton.js
+++ src/CreateTodoButton.js
@@ -1,5 +1,14 @@
+import "./CSS/CreateTodoButton.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
 function CreateTodoButton() {
-	return <input type="button" value="+" />;
+	return (
+		<button className="createTodoButton">
+			Crear nuevo Todo
+			<FontAwesomeIcon icon={faPlus} />
+		</button>
+	);
 }
 
 export { CreateTodoButton };
